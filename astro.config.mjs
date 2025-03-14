94eed571d8467c243fc3c50d3d289bb268f54fb4
davidcortesdev <davidcortes.dev@gmail.com>
2025-03-14 12:30:27 +0100

    vercel fantasy books

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, envField } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    vite: {
-        plugins: [tailwindcss()],
-      },
-    env: {
-      schema: {
-        SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-        SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'})
-      }
+  output: 'server',
+
+  vite: {
+      plugins: [tailwindcss()],
+    },
+
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'})
     }
+  },
+
+  adapter: vercel()
 });
